955995745bc21560dcda86ac4703c08d4caae59c
HBA114 <hbasriayhaner114@gmail.com>
2022-12-21 13:27:49 +0300

    Ready For Use, Tested

--- Program.cs
+++ Program.cs
@@ -4,27 +4,52 @@ using YoutubeExplode.Videos.Streams;
 
 //* For more information visit https://www.nuget.org/packages/YoutubeExplode
 
+Console.WriteLine("Enter path for mp3.txt file(default: /home/hbasri/Documents/)...");
+string? path = Console.ReadLine();
+
+if (path == null || path == "")
+{
+    path = "/home/hbasri/Documents/";
+}
+
+if (!path.Contains(".txt"))
+    path += "mp3.txt";
+
+Console.WriteLine("Enter save path for downloaded files(default: /home/hbasri/Documents/mp3/)...");
+string? savePath = Console.ReadLine();
+
+if (savePath == null || savePath == "")
+{
+    savePath = "/home/hbasri/Documents/mp3/";
+}
+
+if (savePath.Last() != '/') savePath += "/";
+
+string[] links = await File.ReadAllLinesAsync(path);
+
 var youtube = new YoutubeClient();
 
-var video = await youtube.Videos.GetAsync("https://music.youtube.com/watch?v=BX0lKSa_PTk&list=LM");
+foreach (var link in links)
+{
+    var video = await youtube.Videos.GetAsync(link);
 
-var title = video.Title;
-var duration = video.Duration;
+    var title = video.Title;
+    var duration = video.Duration;
 
-Console.WriteLine($"Title: {title}\nDuration: {duration}");
+    title = title.Replace("/", "");
 
-var streamManifest = await youtube.Videos.Streams.GetManifestAsync(
-    "https://music.youtube.com/watch?v=BX0lKSa_PTk&list=LM"
-);
+    Console.WriteLine($"Title: {title}\nDuration: {duration}");
 
-var streamInfo = streamManifest
-    .GetAudioOnlyStreams()
-    .Where(s => s.Container == Container.WebM)
-    .GetWithHighestBitrate();
+    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
 
-var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+    var streamInfo = streamManifest
+        .GetAudioOnlyStreams()
+        .Where(s => s.Container == Container.WebM)
+        .GetWithHighestBitrate();
 
-// await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
-await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{title}.mp3");
+    var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
 
+    // await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
+    await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{savePath}{title}.mp3");
+}
 //TODO: Add Flutter frontend and get all musics from https://music.youtube.com
