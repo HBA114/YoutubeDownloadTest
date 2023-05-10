9fa0ec72e69a111c2daf327adeee47208d3ed0a0
HBA114 <hbasriayhaner114@gmail.com>
2023-05-10 15:54:51 +0300

    Removed unused usings and codes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Xabe.FFmpeg;
-using YoutubeDownloadTest.Helpers;
-using YoutubeExplode;
-using YoutubeExplode.Videos.Streams;
+﻿using YoutubeDownloadTest.Helpers;
 
 //! published with command below:
 
@@ -24,115 +21,11 @@ public class Program
 {
     private static async Task Main(string[] args)
     {
-        #region new code
-
         var parameters = GetPathParameters.GetAllParams(args);
-
-        // Console.WriteLine($"Directory: {parameters._directory}");
-        // Console.WriteLine($"File: {parameters._filePath}");
-        // Console.WriteLine($"Link: {parameters._link}");
-        // Console.WriteLine($"DownloadType: {parameters._downloadType}");
 
         DownloadContent downloadContent = new DownloadContent(parameters);
 
         await downloadContent.StartDownloadAsync();
-
-        #endregion
-
-        #region old code
-        // string? argument1 = null;
-        // string? argument2 = null;
-        // if (args.Count() == 2)
-        // {
-        //     argument1 = args[0];
-        //     argument2 = args[1];
-
-        //     Console.WriteLine($"{argument1}");
-        //     Console.WriteLine($"{argument2}");
-        // }
-
-        // string path = "";
-        // string savePath = "";
-
-        // if (argument1 == null && argument2 == null)
-        // {
-        //     Console.WriteLine("Enter path for mp3.txt file(default: /home/hbasri/Documents/)...");
-        //     path = Console.ReadLine()!;
-
-        //     if (string.IsNullOrEmpty(path))
-        //     {
-        //         path = "/home/hbasri/Documents/";
-        //     }
-        //     Console.WriteLine($"{path}");
-
-        //     if (!path.Contains(".txt"))
-        //         path += "mp3.txt";
-
-        //     Console.WriteLine("Enter save path for downloaded files(default: /home/hbasri/Documents/mp3/)...");
-        //     savePath = Console.ReadLine()!;
-
-        //     if (string.IsNullOrEmpty(savePath))
-        //     {
-        //         savePath = "/home/hbasri/Documents/mp3/";
-        //     }
-        //     Console.WriteLine($"{savePath}");
-        // }
-        // else
-        // {
-        //     path = argument1!;
-        //     savePath = argument2!;
-        // }
-
-        // if (savePath.Last() != '/') savePath += "/";
-
-        // string[] links = await File.ReadAllLinesAsync(path);
-
-        // var youtube = new YoutubeClient();
-
-        // foreach (var link in links)
-        // {
-        //     var video = await youtube.Videos.GetAsync(link);
-
-        //     var title = video.Title;
-        //     var duration = video.Duration;
-
-        //     title = title.Replace("/", "");
-
-        //     Console.WriteLine($"Title: {title}\nDuration: {duration}");
-
-        //     var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
-
-        //     var streamInfo = streamManifest
-        //         .GetAudioOnlyStreams()
-        //         .Where(s => s.Container == Container.Mp4)
-        //         .GetWithHighestBitrate();
-
-        //     var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-
-        //     var mp4_path = $"{savePath}{title}.mp4";
-        //     var mp3_path = $"{savePath}{title}.mp3";
-
-        //     await youtube.Videos.Streams.DownloadAsync(streamInfo, mp4_path);
-
-        //     if (File.Exists(mp3_path)) continue;
-
-        //     try
-        //     {
-        //         var x = await FFmpeg.Conversions.FromSnippet.Convert(mp4_path, mp3_path);
-
-        //         x.SetOutput(mp3_path);
-        //         await x.Start();
-        //         x.Build();
-
-        //         File.Delete(mp4_path);
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         Console.WriteLine($"Error occurred while converting {mp4_path} to {mp3_path}!");
-        //         Console.WriteLine($"Error: {e.Message}");
-        //     }
-        // }
-        #endregion
     }
 }
 
