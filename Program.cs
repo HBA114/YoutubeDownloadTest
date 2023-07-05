8314358c2811eb7e90f6599b6328f22771cb1ff1
HBA114 <hbasriayhaner114@gmail.com>
2023-07-05 17:41:22 +0300

    Added Editorconfig, changed line endings

--- Program.cs
+++ Program.cs
@@ -9,13 +9,11 @@
 // var link = "https://www.youtube.com/watch?v=BX0lKSa_PTk&ab_channel=OliverTree";
 
 /*
-
 dotnet run "/home/hbasri/Downloads" "/home/hbasri/Documents/mp3.txt" "https://music.youtube.com/watch?v=8puHmESPh1g&list=RDAMVM8puHmESPh1g" "mp3"
 dotnet run "/home/hbasri/Downloads" "/home/hbasri/Documents/mp3.txt" "mp3" "https://music.youtube.com/watch?v=8puHmESPh1g&list=RDAMVM8puHmESPh1g"
 dotnet run "/home/hbasri/Documents/mp3.txt" "/home/hbasri/Downloads" "mp3" "https://music.youtube.com/watch?v=8puHmESPh1g&list=RDAMVM8puHmESPh1g"
 dotnet run "/home/hbasri/Documents/mp3.txt" "/home/hbasri/Downloads"
 */
-
 
 public class Program
 {
